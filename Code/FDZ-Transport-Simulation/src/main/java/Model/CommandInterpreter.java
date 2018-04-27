bcbc2e69ed041ddbb1380755b159a2361e8b92c2
nlehmann96 <christophergotmail@gmail.com>
2018-04-27 14:39:33 +0200

    Parser fertig geschrieben

--- Code/FDZ-Transport-Simulation/src/main/java/Model/CommandInterpreter.java
+++ Code/FDZ-Transport-Simulation/src/main/java/Model/CommandInterpreter.java
@@ -16,9 +16,75 @@ public class CommandInterpreter extends Thread {
 
     @Override
     public void run(){
-        /* TODO Interpret String-Command */
+        if (command.substring(0,6).compareTo("STStK00") != 0){
+            /* TODO Command not recognized */
+        }else{
+            char commandNum = command.substring(7,7).toCharArray()[0];
+            switch(commandNum){
+                case '1' : requestEmptyCarriage( stringToInteger( parseLastChars(command,2)));
+
+                case '2' : releaseCarriage( stringToInteger( parseLastChars(command,2)));
+
+                case '3' : repositionCarriage(
+                        stringToInteger(
+                                parseLastChars(command, 4).substring(0,1)) ,
+                        parseLastChars(command,2));
+
+                case '4' : shutdownTransport();
+
+                default: /* TODO Command not recognized*/;
+            }
+        }
     }
 
+/*--PARSER-------------------------------------------------------------------*/
+
+    private String parseLastChars(String aString, int number) {
+        return aString.substring(aString.length()-number, aString.length()-1);
+    }
+
+    private int stringToInteger(String string){
+        /*
+        * returns -1 if String is not a Number or String is
+        * longer than 2, which is not a legit id in FDZ
+        */
+        if(! (string.length()>2)){ //numbers in commands go from 0-99 (2digit)
+            try{
+                int i = Integer.parseInt(string);
+                return i;
+            }catch(NumberFormatException e){return -1;}
+        }
+        return -1;
+    }
+
+/*--COMMAND CONTROL----------------------------------------------------------*/
+/*--REQUEST EMPTY CARRIAGE---------------------------------------------------*/
+/*  STStK001<Message-ID>0002xx  ---------------------------------------------*/
+
+    private void requestEmptyCarriage(int newId){
+
+    }
+
+/*--RELEASE CARRIAGE--------------------------------------------------------*/
+/*  STStK002<Message-ID>0002xx  --------------------------------------------*/
+
+    private void releaseCarriage(int id){
+
+    }
+
+/*--REPOSITION CARRIAGE-----------------------------------------------------*/
+/*  STStK003<Message-ID>0002xxyy  ------------------------------------------*/
+
+    private void repositionCarriage(int id, String position){
+
+    }
+
+/*--SHUTDOWN TRANSPORT SYSTEM-----------------------------------------------*/
+/*  STStK004<Message-ID>0002  ----------------------------------------------*/
+
+    private void shutdownTransport(){
+
+    }
 
 }
 
