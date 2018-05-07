b958c0b0a95752a38f1f5775cb07ff60856e627f
brysiuk <denis-brysiuk@hotmail.de>
2018-05-07 13:32:37 +0200

    Revert "Network"
    This reverts commit 224c2cda92dcc2f30a85a422e08d7582c04949e0.

--- Code/FDZ-Transport-Simulation/src/main/java/Model/FDZNetworkException.java
+++ Code/FDZ-Transport-Simulation/src/main/java/Model/FDZNetworkException.java
@@ -1,28 +1,4 @@
 package Model;
 
-import java.io.IOException;
-
-/**
- *
- * @author therpich
- */
 public class FDZNetworkException {
-    /**
-     * Initializes a FDZNetworkException
-     * @param ex
-     */
-    public FDZNetworkException(IOException ex)
-    {
-        super(ex);
-    }
-
-    /**
-     * Initializes a FDZNetworkException
-     * @param ex
-     */
-    public FDZNetworkException(Exception ex)
-    {
-        super(ex.getLocalizedMessage(), ex);
-    }
-
 }
