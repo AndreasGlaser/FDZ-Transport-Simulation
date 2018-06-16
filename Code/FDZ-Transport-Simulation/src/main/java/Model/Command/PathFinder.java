31c83338beeebfa8d0ac5c117663c1e2db8116c5
nlehmann96 <christophergotmail@gmail.com>
2018-06-16 13:19:16 +0200

    PathFinder Inline Doc und Warnings behoben

--- Code/FDZ-Transport-Simulation/src/main/java/Model/Command/PathFinder.java
+++ Code/FDZ-Transport-Simulation/src/main/java/Model/Command/PathFinder.java
@@ -1,49 +1,72 @@
 package Model.Command;
 
-import Model.Exception.CongestionException;
 import Model.Exception.IllegalSetupException;
 import Model.Station.Station;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-/**@author Noah Lehmann*/
-
+/**
+ * @author Noah Lehmann
+ *
+ * Class to find Paths in Setup for different occasions
+ */
 public class PathFinder {
 
-    private Station blocking;
     private LinkedList<Station> path = new LinkedList<>();
 
-
+    /**
+     * The Constructor to find a Path between two Stations
+     * @param from Beginning of Path
+     * @param to End of Path
+     * @throws IllegalSetupException thrown if an Illegal Setup is detected
+     * @throws NullPointerException thrown if the stations are not known or null
+     */
     public PathFinder(Station from, Station to) throws  IllegalSetupException, NullPointerException {
-        blocking = checkPath(from, to);
-        printPath();
-    }
-    public PathFinder(Station station, int hops) throws  IllegalSetupException, NullPointerException{
-        blocking = checkPath(hops, station);
-        printPath();
+        checkPath(from, to);
     }
 
+    /**
+     * The Constructor to find a Path if only the destination and the distance is known
+     * @param station End of Path
+     * @param hops distance to destination
+     * @throws IllegalSetupException thrown if an Illegal Setup is detected
+     * @throws NullPointerException thrown if the stations are not known or null
+     */
+    public PathFinder(Station station, int hops) throws  IllegalSetupException, NullPointerException{
+        checkPath(hops, station);
+    }
+
+    /**
+     * Getter for final Path that has been found
+     * @return Path requested in one of the Constructors
+     */
     public LinkedList<Station> getPath(){
         return path;
     }
 
-    private Station checkPath(Station from, Station to) throws IllegalSetupException {
+    /**
+     * checks the Stations and calls findRightPathFor(...)
+     * @param from Beginning of Path
+     * @param to End of Path
+     * @throws IllegalSetupException thrown if Illegal Setup has been detected
+     * @throws NullPointerException thrown if Inout Stations are null
+     */
+    private void checkPath(Station from, Station to) throws IllegalSetupException, NullPointerException {
         if(from == null || to == null){
             throw new NullPointerException("Station is null");
         }
         findRightPathFor(to, from, to);
-        for (int i = 0; i < path.size(); i++) {
-            Station station =  path.get(i);
-            if (station != from && station.isCongested()){
-                return station;
-            }
-        }
-        return null;
     }
 
-
-    private Station checkPath(int hops, Station to) throws IllegalSetupException {
+    /**
+     * Checks the given Parameters and finds Path
+     * @param hops distance of Path
+     * @param to End of Path
+     * @throws IllegalSetupException thrown if Illegal Setup has been detected
+     * @throws NullPointerException thrown if Station is null
+     */
+    private void checkPath(int hops, Station to) throws IllegalSetupException, NullPointerException {
         /*returns null if no congestion on way or
          *first station, which was congested in way */
         if(to == null){
@@ -51,74 +74,56 @@ public class PathFinder {
         }
         if(hops == 1 || to.getPrevStations().size() == 0){
             /*END RECURSIVE FUNCTION*/
-            if(to.isCongested()){
-                /*am i congested? 1 more hop to go*/
-                path.addLast(to);
-                return to;
-            }else{
-                /*1 hop back is ok, if i'm not congested*/
-                path.addLast(to);
-                return null;
-            }
+            path.addLast(to);
+            return;
         }
         /*RECURSIVE CALL*/
-        Station prev = checkPath(hops-1, findRightNextHopFor(to));
-        /*WAS PREV-STATION OCCUPIED?*/
+        checkPath(hops-1, findRightNextHopFor(to));
         path.addLast(to);
-        return isOccupied(prev, to);
     }
 
     /* HELPING FUNCTIONS --------------------------------------------------------*/
 
-    private Station findRightNextHopFor(Station station)throws IllegalSetupException {
-        ArrayList<Station> list = station.getPrevStations();
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).getHopsToNewCarriage() == station.getHopsToNewCarriage()-1) return list.get(i);
+    /**
+     * Finds right next hop station back for recursive call of checkPath(int hops, Station to)
+     * @param aStation Station fpr which next hop is needed
+     * @return next hopBack station
+     * @throws IllegalSetupException thrown if Illegal setup has been detected
+     */
+    private Station findRightNextHopFor(Station aStation)throws IllegalSetupException {
+        ArrayList<Station> list = aStation.getPrevStations();
+        for (Station station : list) {
+            if(station.getHopsToNewCarriage() == aStation.getHopsToNewCarriage()-1) return station;
         }
-        throw new IllegalSetupException("Station "+station.getName()+"has no Prev Station with"+
-                "Hops-1 == "+(station.getHopsToNewCarriage()-1));
+        throw new IllegalSetupException("Station "+aStation.getName()+"has no Prev Station with"+
+                "Hops-1 == "+(aStation.getHopsToNewCarriage()-1));
     }
 
+    /**
+     * Finds Right Path for 2 given Stations
+     * @param init Destination Station, never changed in recursive call
+     * @param from Beginning of Path
+     * @param to End of Path, changed in recursive Call
+     * @throws IllegalSetupException If no possible Path could be found
+     */
     private void findRightPathFor(Station init, Station from, Station to) throws IllegalSetupException{
+        if(from == to && to == init && to.getPrevStations().isEmpty()){
+            return; /*Loop detected*/
+        }
         if(to == from) {
             path.addFirst(to);
             return;
-        }
-        if(from == to && to == init && to.getPrevStations().isEmpty()){
-            return; /*Loop detected*/
         }
         for(int i=0; i<to.getPrevStations().size();++i){
             if(path.isEmpty() && to.getPrevStations().get(i)!= init){
                 findRightPathFor(init, from, to.getPrevStations().get(i));
             }
         }
+
         if(!path.isEmpty()){
             path.addLast(to);
-        }else if(from == to && to.getPrevStations().isEmpty()){
+        }else{
             throw new IllegalSetupException("No possible path");
         }
     }
-
-    private Station isOccupied(Station ifYes, Station ifNo){
-        if(ifYes == null){ /*NO*/
-            if(ifNo.isCongested()) {
-                /*AM I OCCUPIED?*/
-                return ifNo; //yes return me
-            }else{
-                return null;//no, no prev Station or me is occupied
-            }
-        }else{           /*YES, PREV-STATION isOccupied*/
-            return ifYes;
-            /*
-             *i don't care if i am occupied, prev station is
-             *causing trouble already, return prev-station
-             */
-        }
-    }
-
-    // TODO: 09.06.18 entferne printpath
-    private void printPath(){
-        path.stream().forEachOrdered(s-> System.out.println("[path] "+s.getName()));
-    }
-
 }
